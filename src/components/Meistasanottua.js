4e27c457d0d0a342cc382ce330bfd2ca4de7a525
imtone1 <88165529+imtone1@users.noreply.github.com>
2023-06-17 13:31:14 +0300

    meistasanottua carousel

--- src/components/Meistasanottua.js
+++ src/components/Meistasanottua.js
@@ -1,4 +1,7 @@
-import React from 'react'
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import { Carousel } from 'react-responsive-carousel';
 import Paaotsikko from '../otsikot/Paaotsikko'
 import { Stack, Box } from '@mui/material'
 import {COLORS} from '../values/colors'
@@ -7,28 +10,53 @@ import tablelasit from '../images/tablelasit.jpg'
 
 
 
+
 const Meistasanottua = () => {
   return (
-   <Box className="boxit"  sx={{backgroundImage: `url(${tablelasit})`, backgroundRepeat: 'no-repeat', backgroundSize:'cover' ,alignItems:'center', justifyContent:'center'}}>
-    <Box sx={{margin:'auto', width:'80%'}}> 
-    <Paaotsikko otsikko={'Meistä sanottua'} teksti={'Asiakastyytyväisyys on meille kunnia-asia.'} />
+//    <Box className="boxit"  sx={{backgroundImage: `url(${tablelasit})`, backgroundRepeat: 'no-repeat', backgroundSize:'cover' ,alignItems:'center', justifyContent:'center'}}>
+//     <Box sx={{margin:'auto', width:'80%'}}> 
+//     <Paaotsikko otsikko={'Meistä sanottua'} teksti={'Asiakastyytyväisyys on meille kunnia-asia.'} />
 
-<Stack direction={{xs:'column', md:'row'}} sx={{alignItems:'center', justifyContent:"space-between", pb:{xs:2,md:8}}}>
+// <Stack direction={{xs:'column', md:'row'}} sx={{alignItems:'center', justifyContent:"space-between", pb:{xs:2,md:8}}}>
 
-        <Palaute teksti={'SapAir Oy ja Konsta olivat todella hyvä yhteistyökumppani meidän kohteessa. Vaikka rakennusvalvonta asetti mitä ihmeellisimpiä ehtoja ja vaatimuksia, jaksoi Konsta neuvotella, suunnitella ja opastaa. Maallikkona oli ilo työskennellä hänen kanssaan, kun ymmärsin mitä puhutaan.Yhteistyö jatkuu tulevissa projekteissa!'}/>
+//         <Palaute teksti={'SapAir Oy ja Konsta olivat todella hyvä yhteistyökumppani meidän kohteessa. Vaikka rakennusvalvonta asetti mitä ihmeellisimpiä ehtoja ja vaatimuksia, jaksoi Konsta neuvotella, suunnitella ja opastaa. Maallikkona oli ilo työskennellä hänen kanssaan, kun ymmärsin mitä puhutaan.Yhteistyö jatkuu tulevissa projekteissa!'}/>
 
-        <Palaute teksti={'Ammattitaitoista, tehokasta ja joustavaa sekä hyvin ystävällistä toimintaa kaikenkaikkiaan. Myös asiakkaan toiveita ja ajatuksia kuunnellen. Suosittelen lämpimästi!'}/>
+//         <Palaute teksti={'Ammattitaitoista, tehokasta ja joustavaa sekä hyvin ystävällistä toimintaa kaikenkaikkiaan. Myös asiakkaan toiveita ja ajatuksia kuunnellen. Suosittelen lämpimästi!'}/>
 
-        <Palaute teksti={'Asiantuntevaa, tehokasta ja nopeaa toimintaa. Kysymyksiin vastataan ja toimeksianto maaliin uskomattoman nopeasti. Apua myös toimeksiannon/työn jälkeen. Vahva suositus.'}/>
-</Stack>
-<Stack direction={{xs:'column', md:'row'}} sx={{alignItems:'center', justifyContent:"space-evenly"}}>
-        <Palaute teksti={'Käynti työmaalle järjestyi nopealla aikataululla ja paikalle saavuttiin sovitusti. Todella ammattitaitoinen ja asiantunteva palvelu. En voi kun lämpimästi suositella yritystä.'}/>
+//         <Palaute teksti={'Asiantuntevaa, tehokasta ja nopeaa toimintaa. Kysymyksiin vastataan ja toimeksianto maaliin uskomattoman nopeasti. Apua myös toimeksiannon/työn jälkeen. Vahva suositus.'}/>
+// </Stack>
+// <Stack direction={{xs:'column', md:'row'}} sx={{alignItems:'center', justifyContent:"space-evenly"}}>
+//         <Palaute teksti={'Käynti työmaalle järjestyi nopealla aikataululla ja paikalle saavuttiin sovitusti. Todella ammattitaitoinen ja asiantunteva palvelu. En voi kun lämpimästi suositella yritystä.'}/>
 
-        <Palaute teksti={'Suosittelemme ehdottomasti yhteistyötä Konstan kanssa. Yhteydenpito on ollut koko ajan sujuvaa ja vastaukset tulevat hetkessä. Asiantuntemus ja vinkit ovat myös olleet erittäin hyödyllisiä.'}/>
-       </Stack>
-       </Box>
+//         <Palaute teksti={'Suosittelemme ehdottomasti yhteistyötä Konstan kanssa. Yhteydenpito on ollut koko ajan sujuvaa ja vastaukset tulevat hetkessä. Asiantuntemus ja vinkit ovat myös olleet erittäin hyödyllisiä.'}/>
+//        </Stack>
+//        </Box>
         
-   </Box>
+//    </Box>
+<Box className="boxit"  sx={{backgroundImage: `url(${tablelasit})`, backgroundRepeat: 'no-repeat', backgroundSize:'cover' ,alignItems:'center', justifyContent:'center'}}>
+<Box sx={{margin:{xs:'auto', sm:5, lg:'auto'}, width:{xs:'80%', md:500}, maxwidth:600}} className='palautebox'> 
+ <Paaotsikko otsikko={'Meistä sanottua'} teksti={'Asiakastyytyväisyys on meille kunnia-asia.'} />
+<Carousel autoPlay stopOnHover infiniteLoop centerMode showStatus={false} interval={5000}  showThumbs={false} centerSlidePercentage={100}>
+<Palaute teksti={'SapAir Oy ja Konsta olivat todella hyvä yhteistyökumppani meidän kohteessa. Vaikka rakennusvalvonta asetti mitä ihmeellisimpiä ehtoja ja vaatimuksia, jaksoi Konsta neuvotella, suunnitella ja opastaa. Maallikkona oli ilo työskennellä hänen kanssaan, kun ymmärsin mitä puhutaan.Yhteistyö jatkuu tulevissa projekteissa!'} rating={5}/>
+
+<Palaute teksti={'Ammattitaitoista, tehokasta ja joustavaa sekä hyvin ystävällistä toimintaa kaikenkaikkiaan. Myös asiakkaan toiveita ja ajatuksia kuunnellen. Suosittelen lämpimästi!'} rating={5} />
+
+<Palaute teksti={'Asiantuntevaa, tehokasta ja nopeaa toimintaa. Kysymyksiin vastataan ja toimeksianto maaliin uskomattoman nopeasti. Apua myös toimeksiannon/työn jälkeen. Vahva suositus.'} rating={5}/>
+
+<Palaute teksti={'Käynti työmaalle järjestyi nopealla aikataululla ja paikalle saavuttiin sovitusti. Todella ammattitaitoinen ja asiantunteva palvelu. En voi kun lämpimästi suositella yritystä.'} rating={5}/>
+
+<Palaute teksti={'Suosittelemme ehdottomasti yhteistyötä Konstan kanssa. Yhteydenpito on ollut koko ajan sujuvaa ja vastaukset tulevat hetkessä. Asiantuntemus ja vinkit ovat myös olleet erittäin hyödyllisiä.' } rating={5}/>
+<Palaute teksti={'Kiitos Konsta, LVi-suunnittelu ja työn valvonta ei olisi paremmin voinneet mennä.'} rating={4}/>
+<Palaute teksti={'Oikein hyvää ja asiantuntevaa palvelua. Iso suositus.'} rating={5}/>
+
+<Palaute teksti={'Nopeaa ja asiantuntevaa osaamista, suosittelen! 👍'} rating={5}/>
+<Palaute teksti={'Asiallinen toimija, suosittelen lämpimästi!'} rating={5}/>
+</Carousel>
+
+</Box>
+        
+</Box>
+
   )
 }
 
--- src/components/Palaute.js
+++ src/components/Palaute.js
@@ -1,19 +1,19 @@
 import React from 'react'
-import {Typography, Box } from '@mui/material'
+import {Typography, Box , Rating} from '@mui/material'
 import { lightGreen } from '@mui/material/colors';
 import FormatQuoteIcon from '@mui/icons-material/FormatQuote';
 
 
-const Palaute = ({teksti}) => {
+const Palaute = ({teksti, rating}) => {
   return (
-    <Box sx={{maxwidth:600}}>
+    <Box sx={{maxwidth:600, textAlign:'center'}}>
     <Box sx={{textAlign:'left'}}>
-      <FormatQuoteIcon color='success'/>
+      <FormatQuoteIcon/>
 </Box>
-<Typography  sx={{ padding:1 }}>
+<Typography  sx={{ padding:1}}>
 {teksti}
 </Typography>
-
+<Rating name="half-rating-read" defaultValue={rating} precision={0.5} readOnly />
 </Box>
   )
 }
