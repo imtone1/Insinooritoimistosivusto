462bde2673d47d94c131c260a2ad5a7e94f7228f
imtone1 <88165529+imtone1@users.noreply.github.com>
2023-06-19 21:37:49 +0300

    lisätty footeriin tieto sivustosta

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import { Box, Stack, Typography } from '@mui/material'
+import { Box, Stack, Typography, Collapse, IconButton } from '@mui/material'
 import logo_SA1 from '../images/logo_SA1.png'
 import jasenyritys from '../images/jasenyritys_2022.png'
 import LK_valkoinen from '../images/LK_valkoinen.png'
@@ -8,12 +8,32 @@ import tietosuoja from '../documents/Tietosuojaseloste.pdf'
 import {ReactComponent as Asiakastietologo}  from '../images/LOGO_SILVER_STANDING_BLACK_FI_433955.svg'
 import PhoneInTalkIcon from '@mui/icons-material/PhoneInTalk';
 import MailIcon from '@mui/icons-material/Mail';
-import ReceiptIcon from '@mui/icons-material/Receipt';
+import { styled } from '@mui/material/styles';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
+
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 
 import {COLORS} from '../values/colors'
 
+const ExpandMore = styled((props) => {
+  const { expand, ...other } = props;
+  return <IconButton {...other} />;
+})(({ theme, expand }) => ({
+  transform: !expand ? 'rotate(0deg)' : 'rotate(180deg)',
+  marginLeft: 'auto',
+  transition: theme.transitions.create('transform', {
+    duration: theme.transitions.duration.shortest,
+  }),
+}));
+
 const Footer = () => {
+  const [expanded, setExpanded] = React.useState(false);
+    
+
+    const handleExpandClick = () => {
+        setExpanded(!expanded);
+
+      };
   return (
     <Box className='boxit' sx={{backgroundColor: COLORS.beige1, overflow:'hidden', margin:'auto'}}>
 
@@ -123,7 +143,43 @@ const Footer = () => {
                 Tietosuojaseloste
             </Typography>
                 </a>
-       
+                <Stack direction={{ xs: 'column', sm: 'row' }} gap={2} sx={{textAlign:'center', paddingTop:2, alignItems:'center', justifyContent:'center'}} >
+            <Stack direction={'row' } >
+              
+            <Typography sx={{ margin:'auto'}}>
+            Tietoa sivustosta
+          </Typography>
+
+          <ExpandMore
+          expand={expanded}
+          onClick={handleExpandClick}
+          aria-expanded={expanded}
+          aria-label="show more"
+          
+        >
+          <ExpandMoreIcon sx={{color:COLORS.white}}/>
+        </ExpandMore>
+
+        <Collapse in={expanded} timeout="auto" unmountOnExit>
+        <Stack direction='column' p={1} pt={3} gap={2} sx={{ alignItems:'flex-start',justifyContent:'flex-start'}}>
+            <Stack direction={{xs:'column', md:'row'}}>
+              <Typography sx={{pr:1}}>
+                  Sivuston suunnitellut ja luonut 
+              </Typography>
+              <Stack direction={'row'} sx={{justifyContent:'center'}}>
+                <Typography variant='h6' sx={{pr:1}}>
+                    Irina Tatti
+                </Typography>
+                <LinkedInIcon onClick={event =>  window.location.href='https://www.linkedin.com/in/irina-tatti/'}/>
+              </Stack>
+            </Stack>
+            <Typography fontSize={'small'}>
+                Kuvat: Adobe Stock
+            </Typography>            
+        </Stack>
+        </Collapse>
+</Stack>
+</Stack>
 <Typography sx={{textAlign:'center', paddingTop:5}}>
     SapAir Oy | Y-tunnus: 3245203-6 | Helmikuja 3, 21310 Vahto
     </Typography>
