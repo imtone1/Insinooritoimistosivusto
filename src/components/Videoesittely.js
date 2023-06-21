68a664c0283bf84f1d2fa73f07e037412ee43e23
imtone1 <88165529+imtone1@users.noreply.github.com>
2023-06-21 21:49:10 +0300

    lisätty video

---
+++ src/components/Videoesittely.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import mainos from '../videos/mainos.webm'
+import { Box } from '@mui/system'
+import {COLORS} from '../values/colors'
+
+const Videoesittely = () => {
+  return (
+
+
+    <div style={{backgroundColor: COLORS.beige1}}>
+        <Box sx={{maxWidth:800, margin:'auto', }}>
+            <video controls width="100%" className="videoPlayer" src={mainos}></video>
+        </Box>
+    </div>
+  
+    )
+}
+
+export default Videoesittely
--- src/pages/Kotisivu.js
+++ src/pages/Kotisivu.js
@@ -2,12 +2,13 @@ import React from 'react'
 import Header from '../components/Header'
 import Palveluitamme from '../components/Palveluitamme'
 import Lupauksemme from '../components/Lupauksemme'
-import Talopalvelu from '../components/Talopalvelu'
+
 
 import Meistasanottua from '../components/Meistasanottua'
 import Otayhteytta from '../components/Otayhteytta'
 import Palvelupolkusteps from '../components/Palvelupolkusteps'
 import Referenssimaara from '../components/Referenssimaara'
+import Videoesittely from '../components/Videoesittely'
 
 
 const Kotisivu = () => {
@@ -19,7 +20,8 @@ const Kotisivu = () => {
       <Palveluitamme/>
       <Referenssimaara/>
       <Lupauksemme/>
-      <Talopalvelu/>
+      <Videoesittely/>
+    
       
       <Palvelupolkusteps/>
       <Meistasanottua/>
