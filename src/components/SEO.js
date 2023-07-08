f24e979b8b026abb4de5d99aa48ffa11b8663221
imtone1 <88165529+imtone1@users.noreply.github.com>
2023-07-08 16:24:26 +0300

    react-snap kokeilu, nopeutui sivusto hieman

--- src/components/SEO.js
+++ src/components/SEO.js
@@ -15,8 +15,21 @@ const SEO =({ title, description } ) => {
       <meta property="og:title" content={title} />
       <meta property="og:description" content={description} />
 
+      <meta property="og:type" content="website" />
+      <meta property="og:url" content="https://esimerkkisaitti.netlify.app/" />
+      {/* <meta property="og:title" content="Instinööritoimisto SapAir Oy - LVI -suunnittelu" />
+      <meta property="og:description" content="LVI-suunnittelu, KVV /IV-työnjohto, projektijohto- ja valvonta tehtävät. Selkeä valinta, kun tarvitset LVIA-suunnittelijaa." />
+      */}
+      <meta property="og:image" content={suurilogo} />
+
+      <meta property="twitter:card" content="summary_large_image" />
+      <meta property="twitter:url" content="https://esimerkkisaitti.netlify.app/" />
+      <meta property="twitter:title" content="Instinööritoimisto SapAir Oy - LVI -suunnittelu" />
+      <meta property="twitter:description" content="LVI-suunnittelu, KVV /IV-työnjohto, projektijohto- ja valvonta tehtävät. Selkeä valinta, kun tarvitset LVIA-suunnittelijaa." />
+      <meta property="twitter:image" content={twitterlogo}/>
+
       {/* <meta property="og:image" content="URL"/>*/}
-      <meta property="og:url" content="https://sapair.fi"/>
+      {/* <meta property="og:url" content="https://sapair.fi"/> */}
       {/* A tag that denotes the name of the Web site in which the shared page resides: */}
       <meta property="og:site_name" content="Insinööritoimisto SapAir Oy - LVI-suunnittelu"/>
       {/* <meta property="og:image" content={process.env.PUBLIC_URL + suurilogo}/> */}
@@ -31,10 +44,10 @@ const SEO =({ title, description } ) => {
 Summary Card with Large Image: Similar to the Summary Card, but with a prominently-featured image.
 App Card: A Card with a direct download to a mobile app.
 Player Card: A Card that can display video/audio/media. */}
-      <meta name="twitter:card" content="summary" />
+      {/* <meta name="twitter:card" content="summary" />
 
       <meta name="twitter:title" content={title} />
-      <meta name="twitter:description" content={description} />
+      <meta name="twitter:description" content={description} /> */}
       {/* <meta name="twitter:image" content={process.env.PUBLIC_URL + twitterlogo} /> */}
       <meta name="twitter:image:alt" content="Insinööritoimisto SapAir Oy - LVI-suunnittelu"/>
       { /* End Twitter tags */ }
--- src/index.js
+++ src/index.js
@@ -1,13 +1,20 @@
 import React from 'react'
+import { hydrate, render } from 'react-dom'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 import App from './App'
 
 
+const APP = (<BrowserRouter>
+  <App />
+</BrowserRouter>)
+
+const rootElement = document.getElementById('root')
+if (rootElement.hasChildNodes()) {
+  hydrate(APP, rootElement)
+} else {
+  render(APP, rootElement)
+}
+
 const root = ReactDOM.createRoot(document.getElementById('root'))
-root.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
-)
